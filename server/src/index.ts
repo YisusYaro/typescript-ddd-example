4a61b238dcd1d744d98bb898355cb6db5785eb2d
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2022-04-24 15:56:59 -0500

    feat: add express server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,26 @@
-console.log('Hello world! ✨');
+import 'reflect-metadata';
+import './app.controller';
+import { AppContainer } from './shared/infraestructure/dependency-injection/app-container';
+import { InversifyExpressServer } from 'inversify-express-utils';
+import { setConfigExpress } from './express.config';
+import { setErrorConfigExpress } from './express.config';
+
+const port = Number(process.env.PORT || 3000);
+
+const container = AppContainer.getInstance().getContainer();
+
+AppContainer.getInstance().setDependencyInjectionApp();
+
+const server = new InversifyExpressServer(container);
+
+server.setConfig((app) => {
+  setConfigExpress(app);
+});
+
+server.setErrorConfig((app) => {
+  setErrorConfigExpress(app);
+});
+
+const app = server.build();
+
+app.listen(port, () => console.log(`Listening on ${port}.`));
