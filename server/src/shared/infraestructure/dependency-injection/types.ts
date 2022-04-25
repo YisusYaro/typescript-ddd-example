e3d7a902a37a116ec0bd311a3b4f4bded11d7337
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2022-04-25 00:42:42 -0500

    feat: add shared infraestructure types

--- server/src/shared/infraestructure/dependency-injection/types.ts
+++ server/src/shared/infraestructure/dependency-injection/types.ts
@@ -1,4 +1,11 @@
 const INFRAESTRUCTURE = {
+  CommandBus: Symbol.for('CommandBus'),
+  QueryBus: Symbol.for('QueryBus'),
+  EventBus: Symbol.for('EventBus'),
+  SqsEventBus: Symbol.for('SqsEventBus'),
+  CommandHandlersInformation: Symbol.for('CommandHandlersInformation'),
+  QueryHandlersInformation: Symbol.for('QueryHandlersInformation'),
+  EventHandlersInformation: Symbol.for('EventHandlersInformation'),
   Logger: Symbol.for('Logger'),
 };
 
