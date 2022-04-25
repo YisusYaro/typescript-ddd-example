e0c99f834e1c636bf7d5251cf41f3f4084f2d726
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2022-04-25 00:40:07 -0500

    feat: add query bus

---
+++ server/src/shared/infraestructure/query-bus/query-handlers-information.ts
@@ -0,0 +1,30 @@
+import { Container, injectable } from 'inversify';
+import { QueryHandler } from '../../application/queries/query.handler';
+import { AppContainer } from '../dependency-injection/app-container';
+import { Query } from '../../application/query';
+import { Result } from '../../application/result';
+import { registerResourcesQueries } from '../../../resources/infraestructure/dependency-injection/register-queries';
+
+export interface QueryHandlersInformation {
+  search(query: Query): QueryHandler<Query, Result>;
+}
+
+@injectable()
+export class QueryHandlersInformationImpl implements QueryHandlersInformation {
+  private container: Container;
+  private queriesHandlersMap: Map<Query, symbol>;
+
+  constructor() {
+    this.container = AppContainer.getInstance().getContainer();
+    this.queriesHandlersMap = new Map();
+    registerResourcesQueries(this.queriesHandlersMap);
+  }
+
+  public search(query: Query): QueryHandler<Query, Result> {
+    const handler = this.queriesHandlersMap.get(query.constructor);
+    if (!handler) throw new Error('Query not registered');
+    const queryHandler =
+      this.container.get<QueryHandler<Query, Result>>(handler);
+    return queryHandler;
+  }
+}
