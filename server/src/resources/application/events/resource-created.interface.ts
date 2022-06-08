ee8549040b994ebefe573131e3d59c917bd31b82
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2022-06-08 09:38:14 -0500

    refactor(resources): remove event handlers

--- server/src/resources/application/events/resource-created.interface.ts
+++
@@ -1,7 +0,0 @@
-import { EventHandler } from '../../../shared/domain/events/event.handler';
-import { ResourceCreatedEvent } from '../../domain/events/resource-created.event';
-
-export interface ResourceCreatedHandler
-  extends EventHandler<ResourceCreatedEvent> {
-  handle(event: ResourceCreatedEvent): Promise<void>;
-}
