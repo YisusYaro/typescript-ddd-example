5b5193de3a999820a17db1ed0d625f68e881c9f9
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2022-06-08 09:40:39 -0500

    refactor: publish events instead handle

--- server/src/shared/infrastructure/event-bus/event-bus.ts
+++ server/src/shared/infrastructure/event-bus/event-bus.ts
@@ -1,21 +1,13 @@
-import { inject, injectable } from 'inversify';
+import { injectable } from 'inversify';
 import { Event } from '../../domain/events/event';
-import { EventHandlersInformation } from './event-handlers-information';
-import { TYPES } from '../dependency-injection/types';
 
 export interface EventBus {
-  execute(event: Event): Promise<any>;
+  publish(event: Event): Promise<void>;
 }
 
 @injectable()
 export class EventBusImpl implements EventBus {
-  constructor(
-    @inject(TYPES.EventHandlersInformation)
-    private eventHandlersInformation: EventHandlersInformation,
-  ) {}
-
-  async execute(event: Event): Promise<any> {
-    const handler = this.eventHandlersInformation.search(event);
-    return await handler.handle(event);
+  async publish(event: Event): Promise<void> {
+    console.log(event);
   }
 }
